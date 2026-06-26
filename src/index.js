45f9bc731e467ef0ee54701ddeec5bacdb0bc53e
Romain Metge <rjametge@gmail.com>
2026-06-25 20:51:32 -0600

    Reorganize project structure - move files to Public and src directories

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import WeatherApp from "./WeatherApp";
-import "./styles/app.css";
+import "./app.css";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
